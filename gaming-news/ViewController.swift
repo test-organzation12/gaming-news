d932f96d350b734bf810e8d8c51df9b22a9184ec
abdulahiroble <abdulahi_2610@hotmail.com>
2021-09-21 14:51:39 +0200

    connect firebase

--- gaming-news/ViewController.swift
+++ gaming-news/ViewController.swift
@@ -15,9 +15,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var saveText: UIButton!
     
+    
+    var db = Firestore.firestore()
+    let notes = "notes"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startListener()
         // Do any additional setup after loading the view.
     }
+    
+    func startListener() {
+        db.collection(notes).addSnapshotListener { snapshot, error in
+            print("hi from firebase")
+        }
+    }
+    
 }
 
