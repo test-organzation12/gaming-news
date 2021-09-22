9e8323d4cb29887b596f2ef2942dfb06ab17af96
abdulahiroble <abdulahi_2610@hotmail.com>
2021-09-22 12:48:09 +0200

    print in console

--- gaming-news/ViewController.swift
+++ gaming-news/ViewController.swift
@@ -15,36 +15,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var saveText: UIButton!
     
-    
-    var db = Firestore.firestore()
-    var notes:[Note] = []
-    var currentNote = -1
+    var parentViewCon:TableViewController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startListener()
-        // Do any additional setup after loading the view.
-    }
-    
-    func startListener() {
-        db.collection("notes").addSnapshotListener {(snap, error) in
-            if let e = error {
-                print("error fetching notes \(e)")
-            }else {
-                if let s = snap{
-                    self.notes.removeAll() // clear array first
-                    for doc in s.documents{
-                        if let txt = doc.data()["txt"] as? String{
-                            print("et dokument: \(txt)")
-                            let note = Note()
-                            note.txt = txt
-                            note.id = doc.documentID
-                            self.notes.append(note)
-                        }
-                    }
-                }
-            }
+        if let parent = parentViewCon {
+            textView.text = parent.notes[parent.currentNote].txt
+        }else {
+            textView.text = "empty..."
         }
+        
+       
+        // Do any additional setup after loading the view.
     }
     
 }
