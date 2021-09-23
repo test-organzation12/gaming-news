1b158e382b7773ada61628ae1178c3e5b7071fff
abdulahiroble <abdulahi_2610@hotmail.com>
2021-09-23 12:26:42 +0200

    add data to firebase

--- gaming-news/ViewController.swift
+++ gaming-news/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var commentView: UITextView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var saveText: UIButton!
     
@@ -19,15 +20,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         if let parent = parentViewCon {
-            textView.text = parent.notes[parent.currentNote].news
+             textView.text = parent.notes[parent.currentNote].title
         }else {
             textView.text = "empty..."
         }
         
+        
+    }
     
     
-        // Do any additional setup after loading the view.
+    @IBAction func savePressed(_ sender: Any) {
+        
+        // update data
+        /*
+        if let p = parentViewCon {
+            let note = p.notes[p.currentNote]
+            note.title = textView.text
+            // p.simpleEdit(note: note)
+        }
+        */
+        
+        // insert data
+        
+        if let x = parentViewCon {
+            let note = x.notes[x.currentNote]
+            note.title = textView.text
+            x.insertData(note: note)
+        }
+        
+        
+        /*
+        if let x = parentViewCon {
+            let note = x.comments[x.currentComment]
+            note.fortnitecomment = commentView.text
+            x.insertCommentData(comment: note)
+        }
+        */
+        
     }
     
 }
